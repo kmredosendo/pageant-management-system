99f86a33cc28c9baab32bd8f8d30e37d33f52073
kmredosendo <kmredosendo@outlook.com>
2025-08-18 12:42:24 +0800

    feat: add rewrites configuration to next.config.ts for Apache request handling

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/pageant/:path*',   // incoming request from Apache
+        destination: '/:path*',    // map to root of Next.js app
+      },
+    ];
+  },
 };
 
 export default nextConfig;
